5b75cd8d45ccc1f6a8c056885d7764cdfa8a6fcc
Basmalh Mokhtar <bsmltmkhtar404@gmail.com>
2026-01-06 18:23:08 +0200

    Upgrade service worker cache to version 2
    Updated cache name and added new files to cache.

--- sw.js
+++ sw.js
@@ -1,34 +1,30 @@
-const cacheName = 'todo-cache-v1';
+const cacheName = 'todo-cache-v2';
+
 const filesToCache = [
-    'index.html',
-    'style.css',
-    'javas.js',
-    'small.png',
-    'large.png',
-    'empty-massage.JPG',
-    'sunset.jpeg'
+    './',
+    './index.html',
+    './style.css',
+    './javas.js',
+    './manifest.json',
+    './small.png',
+    './large.png',
+    './empty-massage.JPG',
+    './sunset.jpeg'
 ];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-        return response || fetch(event.request).catch(() => {
-            // لو أوفلاين والملف مش موجود في cache، ارجع الصفحة الرئيسية
-            return caches.match('index.html');
-        });
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
         })
     );
 });
-
-
-
-
-
-
 
